e2f261d9c572d1f41a66aa6a070daa4cf699c7d3
ds3161 <dsaus@cerner.com>
2020-06-06 13:12:13 -0500

    Full check in

---
+++ src/main/java/com/ds3161/rabbit/test/RabbitTestApplication.java
@@ -0,0 +1,13 @@
+package com.ds3161.rabbit.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitTestApplication.class, args);
+	}
+
+}
